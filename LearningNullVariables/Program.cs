17f7a2d69ee6a9289fe6959cee6dbb0fc358de0a
iloozealot <iloozealot@gmail.com>
2023-11-12 12:44:29 -0600

    Add project files.

---
+++ LearningNullVariables/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+// null - lack of value
+
+//Haven't asked for the age yet
+
+int? age = null;
+
+//I have asked for the age now
+age = 0;
+
+bool? birthday = null;
+double? battingAverage = null;
+decimal? accountBalance = null;
+string? firstName = null;
